caa886f810dc92683d855ee2e37a9c37e525e505
Anthony Diggle <fuseinabowl@gmail.com>
2019-03-17 23:07:37 +0000

    Add use of background batch and attempt to load background resource

--- jumper.py
+++ jumper.py
@@ -1,12 +1,18 @@
 import pyglet
 
 from objects import player, world
+from game import background, resources
+
+def load_background(background_batch):
+    background.load_background(background_batch, resources.background)
 
 def main():
     game_window = pyglet.window.Window(800, 600)
 
     background_batch = pyglet.graphics.Batch()
     main_batch = pyglet.graphics.Batch()
+
+    load_background(background_batch)
 
     instance_world = world.World()
     players = [player.Player()]
