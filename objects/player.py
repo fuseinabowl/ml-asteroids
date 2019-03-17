e8653b246055d224b556613caa9f384cc1820a05
Anthony Diggle <fuseinabowl@gmail.com>
2019-03-17 22:40:48 +0000

    Player returns value from gather... to please pylint

--- objects/player.py
+++ objects/player.py
@@ -2,7 +2,7 @@ import pyglet
 
 class Player():
     def gather_player_perceived_world_state(self, world):
-        return None
+        return 1
 
     def calculate_and_store_actions(self, perceived_world_state):
         pass
