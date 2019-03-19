f5feec34bc636a5f75dd1ab5e8f3d0b9007f45c6
Anthony Diggle <fuseinabowl@gmail.com>
2019-03-19 00:21:43 +0000

    Change to asteroids based game
    Replace forest background with purple starscape, but it doesn't fill the screen yet

--- game/resources.py
+++ game/resources.py
@@ -3,4 +3,4 @@ import pyglet
 pyglet.resource.path = ['resources']
 pyglet.resource.reindex()
 
-background = pyglet.resource.image("backgroundForest.png")
+background = pyglet.resource.image("purple.png")
