3b03a2447920b6880a4778beb5567ee7fc69ddbf
Anthony Diggle <fuseinabowl@gmail.com>
2019-03-20 00:22:27 +0000

    Add momentum (and friction) to the player

--- game/physics_object.py
+++ game/physics_object.py
@@ -1,14 +1,26 @@
+import math
+
 class PhysicsObject():
     def __init__(self, 
             x:float=0,y:float=0,
             x_velocity:float=0,y_velocity:float=0,
             rotation:float=0,
-            rotational_velocity:float=0
+            rotational_velocity:float=0,
+            friction:float=0.01,
+            rotational_friction:float=0.01
     ):
         self.position = [x,y]
         self.velocity = [x_velocity,y_velocity]
         self.rotation = rotation
         self.rotational_velocity =rotational_velocity
+        self.friction = friction
+        self.rotational_friction = rotational_friction
 
     def update(self):
         self.position = [position + velocity for position, velocity in zip(self.position, self.velocity)]
+        self.velocity = [velocity * (1 - self.friction) for velocity in self.velocity]
+
+        new_rotation = self.rotation + self.rotational_velocity
+        self.rotation = new_rotation % (2 * math.pi)
+
+        self.rotational_velocity = self.rotational_velocity * self.rotational_friction
--- game/world.py
+++ game/world.py
@@ -2,11 +2,12 @@ from . import single_frame_actions, physics_object
 
 class World():
     def __init__(self):
+        self._player_maximum_thrust = 0.1
         self._player_ship = physics_object.PhysicsObject(x=50,y=50)
         self._player_controller = None
 
     def update(self, player_actions : single_frame_actions.SingleFrameActions):
-        self._player_ship.velocity[1] = player_actions.thrust
+        self._player_ship.velocity[1] = self._player_ship.velocity[1] + player_actions.thrust * self._player_maximum_thrust
         self._player_ship.update()
 
     def add_player(self, player_controller):
