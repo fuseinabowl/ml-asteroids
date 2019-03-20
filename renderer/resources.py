a5f3c5318bdca5a038b2105c11add18857de74d6
Anthony Diggle <fuseinabowl@gmail.com>
2019-03-20 00:32:45 +0000

    Make ship rotate around its centre

--- renderer/resources.py
+++ renderer/resources.py
@@ -3,5 +3,11 @@ import pyglet
 pyglet.resource.path = ['renderer/resources']
 pyglet.resource.reindex()
 
+def center_image(image : pyglet.resource.image):
+    """Sets an image's anchor point to its center"""
+    image.anchor_x = image.width / 2
+    image.anchor_y = image.height / 2
+
 background = pyglet.resource.image("purple.png")
 player = pyglet.resource.image("player_ship.png")
+center_image(player)
