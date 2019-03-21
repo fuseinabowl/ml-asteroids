0e01785dc2bf34a892d6a7568933bff078d4e446
Anthony Diggle <fuseinabowl@gmail.com>
2019-03-21 00:28:03 +0000

    add 1st pass of asteroid generation

--- game/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import world, player, human_player
--- game/world.py
+++ game/world.py
@@ -1,4 +1,5 @@
-from math import sin, cos
+from math import sin, cos, pi
+from random import random
 
 from . import single_frame_actions, physics_object
 
@@ -16,6 +17,8 @@ class World():
         self._player_ship = physics_object.PhysicsObject(x=50,y=50)
         self._player_controller = None
 
+        self._asteroids = self._create_starting_asteroids()
+
     def update(self, player_actions : single_frame_actions.SingleFrameActions):
         self._player_ship.rotational_velocity = self._player_ship.rotational_velocity + player_actions.turn_speed * (self._player_maximum_turn_thrust + self._thrust_extra_turn_thrust * player_actions.thrust)
         self._player_ship.friction = self._player_base_friction + self._player_thrust_extra_friction * player_actions.thrust
@@ -31,3 +34,66 @@ class World():
     @property
     def player(self):
         return self._player_ship
+
+    def _create_starting_asteroids(self):
+        return [self._create_single_asteroid() for x in range(5)]
+
+    def _create_single_asteroid(self):
+        direction = random() * 2 * pi
+
+        class Borders:
+            LEFT, TOP, RIGHT, BOTTOM = range(4)
+        border_collided_with = Borders.LEFT
+        collision_point = (0,0)
+        
+        # find border in direction from centre
+        left_border_x = 0
+        right_border_x = 800
+        bottom_border_y = 0
+        top_border_y = 600
+
+        if direction > pi:
+            pass
+        elif direction < pi and direction > 0:
+            pass
+        elif direction is 0:
+            border_collided_with = Borders.RIGHT
+            collision_point = (right_border_x, top_border_y / 2)
+        else:
+            border_collided_with = Borders.LEFT
+            collision_point = (0, top_border_y / 2)
+
+        # place asteroid beyond border
+        asteroid_width = 50
+        border_offsets = {
+            Borders.LEFT: (-asteroid_width, 0),
+            Borders.TOP: (0, asteroid_width),
+            Borders.RIGHT: (asteroid_width, 0),
+            Borders.BOTTOM: (0, -asteroid_width)
+        }
+
+        border_offset = border_offsets[border_collided_with]
+
+        asteroid_spawn_location = tuple(collision_coordinate + offset_coordinate for collision_coordinate, offset_coordinate in zip(collision_point, border_offset))
+
+        # make asteroids fly in
+        asteroid_speed = 1
+        def create_velocity_generator(start_angle, end_angle):
+            angle_range = end_angle - start_angle
+            def generate_velocity():
+                chosen_angle = start_angle + random() * angle_range
+                return (sin(chosen_angle) * asteroid_speed, cos(chosen_angle) * asteroid_speed)
+            return generate_velocity
+        border_velocity_generators = {
+            Borders.LEFT: create_velocity_generator    (-1 * pi / 2, 1 * pi / 2),
+            Borders.TOP: create_velocity_generator     ( 1 * pi / 2, 3 * pi / 2),
+            Borders.RIGHT: create_velocity_generator   ( 3 * pi / 2, 5 * pi / 2),
+            Borders.BOTTOM: create_velocity_generator  ( 5 * pi / 2, 7 *pi / 2)
+        }
+        velocity = border_velocity_generators[border_collided_with]()
+
+        new_asteroid = physics_object.PhysicsObject(
+            x=asteroid_spawn_location[0], y=asteroid_spawn_location[1],
+            x_velocity=velocity[0], y_velocity=velocity[1]
+        )
+        return new_asteroid
