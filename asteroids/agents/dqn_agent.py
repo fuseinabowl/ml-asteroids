ef083d54a7dadd7eff990f11a4a3d458ca6a1c46
Anthony Diggle <fuseinabowl@gmail.com>
2019-05-27 21:44:48 +0100

    Fix softmax problems
    In softmax, if the mantissa is in the 0-1 range the predictions' probabilities are inverted, so we're more likely to select bad paths. At 1 it is completely uniform, only when we get to 1+ do we start selecting for expected reward.

--- asteroids/agents/dqn_agent.py
+++ asteroids/agents/dqn_agent.py
@@ -17,7 +17,7 @@ class DQNAgent:
         self.action_size = action_size
         self.gamma = 0.95    # discount rate
         self.learning_rate = 0.001
-        self.action_probability_sharpening = 0
+        self.action_probability_sharpening = 1
         self.action_probability_sharpening_increase = 0.05
         self.action_probability_sharpening_max = 5
         self.tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
